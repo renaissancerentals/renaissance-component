d82228086be779d13e6c671fa44503d2e0c83aac
asikpradhan <asikpradhan@icloud.com>
2024-02-15 07:20:31 -0500

    feat: map embed parameters

--- src/map/MapSection.tsx
+++ src/map/MapSection.tsx
@@ -6,6 +6,8 @@ export const MapSection: React.FC<MapSectionProps> = ({src}) => {
         <section className="section-map">
             <iframe title="map"
                     src={src}
+                    loading="lazy"
+                    referrerPolicy="no-referrer-when-downgrade"
                     allowFullScreen
             />
         </section>
