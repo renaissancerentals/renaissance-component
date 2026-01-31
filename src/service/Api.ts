658d979a5777e36a1d5ece775cfea50abfced0ee
contentmuncher <contentmunch@gmail.com>
2026-01-31 13:18:04 -0500

    chore: update base urls

--- src/service/Api.ts
+++ src/service/Api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const RENAISSANCE_BASE_URL = process.env.REACT_APP_BASE_URL ? process.env.REACT_APP_BASE_URL : "https://www.veronaparkneighborhood.com/";
+export const RENAISSANCE_BASE_URL = process.env.REACT_APP_BASE_URL ? process.env.REACT_APP_BASE_URL : "https://www.scholarsrooftop.com/";
 
 export default axios.create({
     baseURL: RENAISSANCE_BASE_URL + "api/"
