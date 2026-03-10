ee1fae3ae1ad0e8a8891c6737e02d476d01ed801
contentmuncher <contentmunch@gmail.com>
2026-03-10 15:15:46 -0400

    feat: new asset api

--- src/asset/service/AssetService.ts
+++ src/asset/service/AssetService.ts
@@ -2,18 +2,17 @@ import {extractIdFrom, isGoogleDriveImage} from "../../utils/Utils";
 import {Asset} from "../data/Asset";
 import Api from "../../service/Api";
 
-const ASSET_DOWNLOAD_FRAGMENT = "api/asset/download/";
-
 export const propertyFragment = (propertyId?: string): string => propertyId ? propertyId + "/" : "";
 export const getAssetUrl = (imageUrl: string, assetGatewayId?: string): string => {
     if (isGoogleDriveImage(imageUrl)) {
 
-        return propertyIdToDomain() + ASSET_DOWNLOAD_FRAGMENT + extractIdFrom(imageUrl);
+        return propertyIdToDomain() + "api/assets/" + extractIdFrom(imageUrl) + "/download";
     }
     return imageUrl;
 };
 
-export const assetUrlFrom = (id: string, assetGatewayId: string): string => propertyIdToDomain(propertyFragment(assetGatewayId)) + ASSET_DOWNLOAD_FRAGMENT + id;
+export const assetUrlFrom = (id: string, assetGatewayId: string): string => propertyIdToDomain(propertyFragment(assetGatewayId)) + "api/assets/" + id + "/download";
+;
 
 
 // export const getAssetsFrom = async (folderId: string): Promise<Asset[]> => {
@@ -31,17 +30,16 @@ export const getAssetsFrom = (folderId: string): Promise<Asset[]> => {
             console.log(reason);
         });
 }
-
+export const assetDomains: string[] = process.env.REACT_APP_ASSET_BASE_URLS ? process.env.REACT_APP_ASSET_BASE_URLS.split(",") : [
+    "https://www.veronaparkneighborhood.com/",
+    "https://www.covenanterhill.com/",
+    "https://www.highgrovebloomington.com/",
+    "https://www.scholarsquad.com/",
+    "https://www.summerhouseatindiana.com/",
+]
 
 export const propertyIdToDomain = (propertyId?: string): string => {
-    const assetDomains = [
-        "https://www.veronaparkneighborhood.com/",
-        "https://www.covenanterhill.com/",
-        "https://www.highgrovebloomington.com/",
-        "https://www.scholarsquad.com/",
-        "https://www.summerhouseatindiana.com/",
-        "https://www.veronaparkneighborhood.com/"
-    ];
+
     const random = Math.ceil(Math.random() * 6) - 1;
 
     return random < 0 || random > assetDomains.length - 1 ? assetDomains[0] : assetDomains[random];
